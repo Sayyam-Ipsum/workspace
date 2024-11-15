b7fa408fb98cfe52d251617b794b76f9e7ff5b66
Sayyam-Ipsum <ahmed.sayyam@ipsum.co.uk>
2024-11-15 20:56:32 +0500

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,7 @@
                          <img src="{{ url('assets/img/search-icon.svg')}}" alt="">
                     </button>
                 </div>
-                <input type="text" class="search-input border-0 small"
+                <input type="text" class="search-input text-white border-0 small"
                     aria-label="Search" aria-describedby="basic-addon2">
 
             </div>
